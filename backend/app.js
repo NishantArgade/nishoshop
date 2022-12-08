805b1f37d0bf47065206b34f0c920790bff8c321
Nishant Argade <nishantargade4579@gmail.com>
2022-12-08 11:05:53 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -35,5 +35,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
 });
 // Middleware for (routes error handles)Errors
+app.use(errorMiddleware);
 
 module.exports = app;
